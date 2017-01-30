f6cfed73028fc307fe8a422f9c8883774eea2b07
i.gonchar <IgorGoncharUA@gmail.com>
2017-01-30 17:30:11 +0200

    FightVsEnemy.java was added

--- src/main/java/war/FightVsEnemy.java
+++ src/main/java/war/FightVsEnemy.java
@@ -1,7 +1,35 @@
 package war;
 
+import army.Mage;
+import army.Warrior;
+
 public class FightVsEnemy {
     private int enemyWarriors = 50;
     private int enemyMages = 50;
+
+    private int yourWarriors;
+    private int yourMages;
+
+    public boolean fight() {
+        boolean victory = false;
+        yourWarriors = Warrior.warriorCount;
+        yourMages = Mage.mageCount;
+
+        if (yourWarriors >= enemyWarriors && yourMages >= enemyMages) {
+            victory = true;
+        } else if (yourWarriors < enemyWarriors && yourMages < enemyMages) {
+            victory = false;
+        }
+
+        return victory;
+    }
+
+    private int warriorsFight(int yourWarriors, int enemyWarriors) {
+        return yourWarriors - enemyWarriors;
+    }
+
+    private int magesFight(int yourMages, int enemyMages) {
+        return yourMages - enemyMages;
+    }
 
 }
