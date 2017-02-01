3083c3e31eb95e25897314d64b5cf832695e8f18
i.gonchar <IgorGoncharUA@gmail.com>
2017-02-01 18:32:15 +0200

    heavy warriors were added

--- src/main/java/army/HeavyWarrior.java
+++ src/main/java/army/HeavyWarrior.java
@@ -1,7 +1,27 @@
 package army;
 
+import GUI.swing.MainWindow;
+
 /**
  * Created by Home on 12.12.2015.
  */
 public class HeavyWarrior extends Warrior {
+    private static int heavyWarriorCount = 0;
+
+    public HeavyWarrior() {
+        int allFreeWarriors = getFreeWarriorsCount();
+        setFreeWarriorsCount(allFreeWarriors - 1);
+        heavyWarriorCount++;
+
+        // For GUI
+        MainWindow.setWarrToGui();
+    }
+
+    public static int getHeavyWarriorCount() {
+        return heavyWarriorCount;
+    }
+
+    public static void setHeavyWarriorCount(int heavyWarriorCount) {
+        HeavyWarrior.heavyWarriorCount = heavyWarriorCount;
+    }
 }
--- src/main/java/army/Warrior.java
+++ src/main/java/army/Warrior.java
@@ -7,12 +7,30 @@ import GUI.swing.MainWindow;
  */
 public class Warrior extends Unit {
     public static int warriorCount = 0;
+    private static int freeWarriorsCount = 0;
 
     public Warrior() {
         int allUnits = getAllUnits();
         setAllUnits(allUnits + 1);
         warriorCount++;
+        freeWarriorsCount++;
         // For GUI
         MainWindow.setWarrToGui();
+    }
+
+    public static int getWarriorCount() {
+        return warriorCount;
+    }
+
+    public static void setWarriorCount(int warriorCount) {
+        Warrior.warriorCount = warriorCount;
+    }
+
+    public static int getFreeWarriorsCount() {
+        return freeWarriorsCount;
+    }
+
+    public static void setFreeWarriorsCount(int freeWarriorsCount) {
+        Warrior.freeWarriorsCount = freeWarriorsCount;
     }
 }
